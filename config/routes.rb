43b7e99f1b93aa503654e60bb1250b0ef7b67f87
R. Joe Hatfield <rjhat9212@gmail.com>
2013-06-11 22:39:47 -0700

    Added some simple rest index methods, one secure, one not

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,16 @@ R3bdc::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   resources :users
+
+  #Restful API providing access to the users
+  namespace :rest do
+    namespace :secure do
+      namespace :v1 do
+        resource :users, :defaults => { :format => 'json' }
+      end
+    end
+    namespace :v1 do
+      resources :users , :defaults => { :format => 'json' }
+    end
+  end
 end
